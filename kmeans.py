c8530d335dbee8ec0e25939910a91278eb13740a
dsouzaj98 <66758205+dsouzaj98@users.noreply.github.com>
2020-08-25 17:36:50 -0400

    update model

--- kmeans.py
+++ kmeans.py
@@ -1,9 +1,71 @@
 from sklearn.preprocessing import Normalizer
 from mywork import *
-us_df=pd.concat(us_df)
-close_data=us_df["Adj Close"]
-open_data=us_df["Open"]
+from sklearn.pipeline import make_pipeline
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
 
-print(close_data.iloc[0])
 normalizer=Normalizer()
+new=normalizer.fit_transform(movements)
 
+
+# china_new=normalizer.fit_transform(china_movements)
+
+
+# fig, axs=plt.subplots(3,5)
+# axs=axs.ravel()
+# for i in range(15):
+#     axs[i].plot(us_movements[i][:])
+#     plt.title(us_companies[i])
+#     plt.show()
+
+kmeans=KMeans(n_clusters=2, max_iter=1000)
+pipeline=make_pipeline(normalizer, kmeans)
+pipeline.fit(movements)
+labels=pipeline.predict(movements)
+df=pd.DataFrame({'labels':labels, "companies":companies})
+
+reduced_data=PCA(n_components=2).fit_transform(new)
+kmeans2=KMeans(n_clusters=2)
+kmeans2.fit(reduced_data)
+labels=kmeans2.predict(reduced_data)
+df2=pd.DataFrame({'labels':labels, "companies":companies})
+print(df2.sort_values('labels'))
+
+
+
+h = 0.01
+
+# plot the decision boundary
+x_min, x_max = reduced_data[:, 0].min() - 1, reduced_data[:,0].max() + 1
+y_min, y_max = reduced_data[:, 1].min() - 1, reduced_data[:,1].max() + 1
+xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+
+# Obtain abels for each point in the mesh using our trained model
+Z = kmeans2.predict(np.c_[xx.ravel(), yy.ravel()])
+
+# Put the result into a color plot
+Z = Z.reshape(xx.shape)
+
+# define colorplot
+cmap = plt.cm.Paired
+
+# plot figure
+plt.clf()
+plt.figure(figsize=(10,10))
+plt.imshow(Z, interpolation='nearest',
+ extent = (xx.min(), xx.max(), yy.min(), yy.max()),
+ cmap = cmap,
+ aspect = 'auto', origin='lower')
+plt.plot(reduced_data[:, 0], reduced_data[:, 1], 'k.', markersize=5)
+
+
+# plot the centroid of each cluster as a white X
+centroids = kmeans.cluster_centers_
+plt.scatter(centroids[:, 0], centroids[:, 1],
+ marker='x', s=169, linewidth=3,
+ color='w', zorder=10)
+
+plt.title('K-Means Clustering on Stock Market Movements (PCA-Reduced Data)')
+plt.xlim(x_min, x_max)
+plt.ylim(y_min, y_max)
+plt.show()
--- mywork.py
+++ mywork.py
@@ -31,20 +31,24 @@ us_dict={
     }
 us_companies=sorted(us_dict.items(), key=lambda x: x[1])
 us_data=web.DataReader(list(us_dict.values()), data_source, start_date, end_date)
-us_close=np.array(us_data["Adj Close"]).T
-us_open=np.array(us_data["Open"]).T
+us_close=us_data["Close"]
+us_open=us_data["Open"]
+us_close=np.array(us_close).T
+us_open=np.array(us_open).T 
+
 
 
 
 us_rows, us_cols=us_close.shape
 
-us_movements=np.zeros([us_rows,us_cols])
+us_movements=np.zeros([us_rows,900])
 
 for i in range(0, us_rows):
-    us_movements[i,:]=np.subtract(us_close[i,:], us_open[i,:])
+    us_movements[i,:]=np.subtract(us_close[i,:900], us_open[i,:900])
 
-for i in range(0, len(us_companies)):
-    print('Company: {}, Change: {}'.format(us_companies[i][0], sum(us_movements[i][:])))
+
+# for i in range(0, len(us_companies)):
+#     print('Company: {}, Change: {}'.format(us_companies[i][0], sum(us_movements[i][:])))
 
 china_dict={
     "Tencent":"0700.HK", 
@@ -65,10 +69,22 @@ china_dict={
 }
 china_companies=sorted(china_dict.items(), key=lambda x: x[1])
 china_data=web.DataReader(list(china_dict.values()), data_source, start_date, end_date)
-china_close=np.array(china_data["Adj Close"]).T
+china_close=np.array(china_data["Close"]).T
 china_open=np.array(us_data["Open"]).T
 
+china_rows, china_cols=china_close.shape
 
+china_movements=np.zeros([china_rows,china_cols])
+
+for i in range(0, china_rows):
+    china_movements[i,:]=np.subtract(china_close[i,:], china_open[i,:900])
+
+col_mean=np.nanmean(china_movements, axis=0)
+inds=np.where(np.isnan(china_movements))
+china_movements[inds]=np.take(col_mean, inds[1])
+
+movements=np.concatenate((us_movements, china_movements), axis=0)
+companies=us_companies+china_companies
 
 
 
