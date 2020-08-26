b7e25ae11026af13bf09ccb4790d4eb196a68ac7
dsouzaj98 <66758205+dsouzaj98@users.noreply.github.com>
2020-08-26 17:00:58 -0400

    add testing function

--- kmeans.py
+++ kmeans.py
@@ -4,11 +4,6 @@ from sklearn.pipeline import make_pipeline
 from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
 
-normalizer=Normalizer()
-new=normalizer.fit_transform(movements)
-
-
-# china_new=normalizer.fit_transform(china_movements)
 
 
 # fig, axs=plt.subplots(3,5)
@@ -18,54 +13,61 @@ new=normalizer.fit_transform(movements)
 #     plt.title(us_companies[i])
 #     plt.show()
 
-kmeans=KMeans(n_clusters=2, max_iter=1000)
-pipeline=make_pipeline(normalizer, kmeans)
-pipeline.fit(movements)
-labels=pipeline.predict(movements)
-df=pd.DataFrame({'labels':labels, "companies":companies})
+#implement Kmeans
+def km(movements, clusters=2):
+    normalizer=Normalizer()
+    kmeans=KMeans(n_clusters=clusters, max_iter=1000)
+    pipeline=make_pipeline(normalizer, kmeans)
+    pipeline.fit(movements)
+    labels=pipeline.predict(movements)
+    df=pd.DataFrame({'labels':labels, "companies":companies})
+    return (df.sort_values('labels'))
 
-reduced_data=PCA(n_components=2).fit_transform(new)
-kmeans2=KMeans(n_clusters=2)
-kmeans2.fit(reduced_data)
-labels=kmeans2.predict(reduced_data)
-df2=pd.DataFrame({'labels':labels, "companies":companies})
-print(df2.sort_values('labels'))
+#implement PCA
+def pca(movements, clusters=2):
+    normalizer=Normalizer()
+    new=normalizer.fit_transform(movements)
+    reduced_data=PCA(n_components=2).fit_transform(new)
+    kmeans=KMeans(n_clusters=2)
+    kmeans.fit(reduced_data)
+    labels=kmeans.predict(reduced_data)
+    df=pd.DataFrame({'labels':labels, "companies":companies})
+    h = 0.01
+
+    # plot the decision boundary
+    x_min, x_max = reduced_data[:, 0].min() - 1, reduced_data[:,0].max() + 1
+    y_min, y_max = reduced_data[:, 1].min() - 1, reduced_data[:,1].max() + 1
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+
+    # Obtain abels for each point in the mesh using our trained model
+    Z = kmeans.predict(np.c_[xx.ravel(), yy.ravel()])
+
+    # Put the result into a color plot
+    Z = Z.reshape(xx.shape)
+
+    # define colorplot
+    cmap = plt.cm.Paired
+
+    # plot figure
+    plt.clf()
+    plt.figure(figsize=(10,10))
+    plt.imshow(Z, interpolation='nearest',
+    extent = (xx.min(), xx.max(), yy.min(), yy.max()),
+    cmap = cmap,
+    aspect = 'auto', origin='lower')
+    plt.plot(reduced_data[:, 0], reduced_data[:, 1], 'k.', markersize=5)
+
+
+    # plot the centroid of each cluster as a white X
+    centroids = kmeans.cluster_centers_
+    plt.scatter(centroids[:, 0], centroids[:, 1],
+    marker='x', s=169, linewidth=3,
+    color='w', zorder=10)
+
+    plt.title('K-Means Clustering on Stock Market Movements (PCA-Reduced Data)')
+    plt.xlim(x_min, x_max)
+    plt.ylim(y_min, y_max)
+    plt.show()
 
 
 
-h = 0.01
-
-# plot the decision boundary
-x_min, x_max = reduced_data[:, 0].min() - 1, reduced_data[:,0].max() + 1
-y_min, y_max = reduced_data[:, 1].min() - 1, reduced_data[:,1].max() + 1
-xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
-
-# Obtain abels for each point in the mesh using our trained model
-Z = kmeans2.predict(np.c_[xx.ravel(), yy.ravel()])
-
-# Put the result into a color plot
-Z = Z.reshape(xx.shape)
-
-# define colorplot
-cmap = plt.cm.Paired
-
-# plot figure
-plt.clf()
-plt.figure(figsize=(10,10))
-plt.imshow(Z, interpolation='nearest',
- extent = (xx.min(), xx.max(), yy.min(), yy.max()),
- cmap = cmap,
- aspect = 'auto', origin='lower')
-plt.plot(reduced_data[:, 0], reduced_data[:, 1], 'k.', markersize=5)
-
-
-# plot the centroid of each cluster as a white X
-centroids = kmeans.cluster_centers_
-plt.scatter(centroids[:, 0], centroids[:, 1],
- marker='x', s=169, linewidth=3,
- color='w', zorder=10)
-
-plt.title('K-Means Clustering on Stock Market Movements (PCA-Reduced Data)')
-plt.xlim(x_min, x_max)
-plt.ylim(y_min, y_max)
-plt.show()
--- test.py
+++ test.py
@@ -1,19 +1,26 @@
 import pandas_datareader.data as web
 import numpy as np
-def testing(key, val):
+from sklearn.preprocessing import Normalizer
+from sklearn.pipeline import make_pipeline
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+from kmeans import movements, km
+def testing(key, val, clusters=2):
 
     data_source='yahoo'
-
     start_date='2017-01-01'
     end_date='2020-08-24'
     test_dict={key:val}
-    test_data=web.DataReader(list(us_dict.values())),data_source, start_date, end_date
+    test_data=web.DataReader(val,data_source, start_date, end_date)
     test_open=test_data["Open"]
     test_close=test_data["Close"]
     test_open=np.array(test_open).T 
     test_close=np.array(test_close).T 
-    row, col=test_close.shape
-    movement=np.zeros([row, col])
-    for i in range(0, row):
-        movement[i,:]=np.subtract(us_close[i,:], us_open[i,:])
+    test_movement=np.subtract(test_close[:900], test_open[:900])
+    new_movements=np.append(movements, test_movement, axis=0)
+    x=km(new_movements)
+    return (movements.shape)
+    
+print(testing('Tesla', 'TSLA'))
+
     
