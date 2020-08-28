1124a1d9658b38e4ecf8b8d65ccb568055087f83
dsouzaj98 <66758205+dsouzaj98@users.noreply.github.com>
2020-08-28 16:40:49 -0400

    add src folder

---
+++ src/cleaning.py
@@ -0,0 +1,85 @@
+import pandas as pd 
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import matplotlib.cbook as cbook 
+import numpy as np
+import datetime
+import pandas_datareader.data as web
+
+# defines data source and parameters for search
+data_source='yahoo'
+
+start_date='2017-01-01'
+end_date='2020-08-24'
+
+#creates data table for U.S. companies
+us_dict={
+    'Amazon':'AMZN',
+    'Apple': 'AAPL',
+    'Berkshire Hathaway':'BRK-B',
+    'Facebook': 'FB',
+    'Google':'Googl',
+    'Home Depot':'HD',
+    'Intel':'INTC',
+    'Johnson and Johnson':'JNJ',
+    'JP Morgan Chase':'JPM',
+    'Mastercard':"MA",
+    "Microsoft":"MSFT",
+    "Proctor and Gamble":'PG',
+    'United Healthcare': 'UNH',
+    'Visa':'V',
+    "Verizon":'VZ'
+    }
+us_companies=sorted(us_dict.items(), key=lambda x: x[1])
+us_data=web.DataReader(list(us_dict.values()), data_source, start_date, end_date)
+us_close=us_data["Close"]
+us_open=us_data["Open"]
+us_close=np.array(us_close).T
+us_open=np.array(us_open).T 
+
+us_rows, us_cols=us_close.shape
+
+us_movements=np.zeros([us_rows,900])
+
+for i in range(0, us_rows):
+    us_movements[i,:]=np.subtract(us_close[i,:900], us_open[i,:900])
+
+# creates data table for Chinese companies
+china_dict={
+    "Tencent":"0700.HK", 
+    "HSBC":"0005.HK",
+    "CCB":"0939.HK",
+    "China Mobile":"0941.HK",
+    "ABC":"1288.HK",
+    "AIA":"1299.HK",
+    "ICBC":"1398.HK",
+    'PSBC':"1658.HK",
+    "PING AN":"2318.HK",
+    "CHINA LIFE":"2628.HK", 
+    "Meituan":"3690.HK",
+    "CM BANK":"3698.HK",
+    "Bank of China":"3988.HK",
+    "JD.com":"9618.HK",
+    "Alibaba":"9988.HK"
+}
+china_companies=sorted(china_dict.items(), key=lambda x: x[1])
+china_data=web.DataReader(list(china_dict.values()), data_source, start_date, end_date)
+china_close=np.array(china_data["Close"]).T
+china_open=np.array(china_data["Open"]).T
+
+china_rows, china_cols=china_close.shape
+china_movements=np.zeros([china_rows,china_cols])
+for i in range(0, china_rows):
+    china_movements[i,:]=np.subtract(china_close[i,:], china_open[i,:900])
+
+#deals with nan values in China table
+col_mean=np.nanmean(china_movements, axis=0)
+inds=np.where(np.isnan(china_movements))
+china_movements[inds]=np.take(col_mean, inds[1])
+
+movements=np.concatenate((us_movements, china_movements), axis=0)
+companies=us_companies+china_companies
+
+
+
+
---
+++ src/kmeans.py
@@ -0,0 +1,103 @@
+from sklearn.preprocessing import Normalizer
+from cleaning import *
+from sklearn.pipeline import make_pipeline
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+from tomark import Tomark 
+import matplotlib.gridspec as gs 
+
+def plot_us():
+    us_mv_dict={}
+    for i in range(15):
+        plt.figure()
+        us_mv_dict[us_companies[i]]=np.mean(us_movements[i])
+        plt.plot(us_data[:900].index,us_movements[i])
+        plt.title(us_companies[i])
+        plt.ylabel("Stock Price Movement (USD)")
+        plt.savefig(f"Images/movements/{us_companies[i]}.png")
+    return us_mv_dict
+def plot_china():
+    c_mv_dict={}
+    for i in range(15):
+        plt.figure()
+        c_mv_dict[china_companies[i]]=np.mean(china_movements[i])
+        plt.plot(china_data[:900].index,china_movements[i])
+        plt.title(china_companies[i])
+        plt.ylabel('Stock Price Movement (USD)')
+        plt.savefig(f"Images/movements/{china_companies[i]}.png")
+    return c_mv_dict
+def plot_movements():
+    mv_dict={}
+    for i in range(30):
+        mv_dict[companies[i]]=np.mean(movements[i])
+        plt.plot(us_data[:900].index, movements[i])
+        plt.title(companies[i])
+        #plt.savefig(f"Images/movements/{companies[i]}.png")
+    return mv_dict
+
+#implement Kmeans
+def km(movements, companies=companies, clusters=2):
+    normalizer=Normalizer()
+    kmeans=KMeans(n_clusters=clusters, max_iter=1000)
+    pipeline=make_pipeline(normalizer, kmeans)
+    pipeline.fit(movements)
+    labels=pipeline.predict(movements)
+    df=pd.DataFrame({'labels':labels, "companies":companies})
+    return (df.sort_values('labels'))
+
+#implement PCA
+def pca(movements,companies=companies, clusters=2):
+    normalizer=Normalizer()
+    new=normalizer.fit_transform(movements)
+    reduced_data=PCA(n_components=2).fit_transform(new)
+    kmeans=KMeans(n_clusters=2)
+    kmeans.fit(reduced_data)
+    labels=kmeans.predict(reduced_data)
+    df=pd.DataFrame({'labels':labels, "companies":companies})
+    
+    #determine size of meshplot
+    h = 0.01
+
+    # plot the decision boundary
+    x_min, x_max = reduced_data[:, 0].min() - 1, reduced_data[:,0].max() + 1
+    y_min, y_max = reduced_data[:, 1].min() - 1, reduced_data[:,1].max() + 1
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+
+    # Obtain labels for each point 
+    Z = kmeans.predict(np.c_[xx.ravel(), yy.ravel()])
+
+    # Placing in plot
+    Z = Z.reshape(xx.shape)
+
+    # define colorplot
+    cmap = plt.cm.Paired
+
+    # plot figure
+    plt.clf()
+    plt.figure(figsize=(10,10))
+    plt.imshow(Z, interpolation='nearest',
+    extent = (xx.min(), xx.max(), yy.min(), yy.max()),
+    cmap = cmap,
+    aspect = 'auto', origin='lower')
+    plt.plot(reduced_data[:, 0], reduced_data[:, 1], 'k.', markersize=5)
+
+
+    # plot the centroids for each cluster
+    centroids = kmeans.cluster_centers_
+    plt.scatter(centroids[:, 0], centroids[:, 1],
+    marker='x', s=169, linewidth=3,
+    color='w', zorder=10)
+
+    plt.title('K-Means Clustering on Stock Market Movements (PCA-Reduced Data)', fontdict={'fontsize':18})
+    plt.text(.75,.75, "USA", fontdict={'fontsize':18})
+    plt.text(-1,.75, "China", fontdict={'fontsize':18})
+    plt.xlim(x_min, x_max)
+    plt.ylim(y_min, y_max)
+    plt.savefig("Images/clustering_pca.png")
+    plt.show()
+    return(df.sort_values('labels'))
+
+
+# df1=pd.DataFrame.from_dict(plot_movements(), orient='index')
+# print(df1.to_markdown())
+
